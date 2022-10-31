97e28c12a2069ccd233564a21d6171f54873ae7b
liuethanc <115133002+liuethanc@users.noreply.github.com>
2022-10-30 23:04:45 -0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let icon = document.getElementsByClassName("icon")[0];
-
-icon.addEventListener('click', myFunction);
-
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
